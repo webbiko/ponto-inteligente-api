b5dbceb054fb58921f853157c35d58e0a6cd8b18
Wagner Batista <wagner.batista@deposit-solutions.com>
2017-10-22 12:24:20 +0200

    Initial code of project

---
+++ src/main/java/com/webbiko/pontointeligente/api/PontoInteligenteApplication.java
@@ -0,0 +1,11 @@
+package com.webbiko.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligenteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PontoInteligenteApplication.class, args);
+    }
+}
